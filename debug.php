654e5eb60239cef74ba696e561a89315cbe85c61
Duhan Muhammad Aziz <emailkamu@gmail.com>
2026-07-29 11:54:54 +0700

    Cleanup: Remove unused test and debug files

--- debug.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$db = App\Core\Database::getInstance()->getConnection();
-$stmt = $db->query("DESCRIBE roll_payments");
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
--- public/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-require __DIR__ . '/../app/Core/Database.php';
-$db = \App\Core\Database::getInstance()->getConnection();
-$stmt = $db->query("SELECT id, username, nama_lengkap, photo FROM roll_users");
-print_r($stmt->fetchAll(\PDO::FETCH_ASSOC));
